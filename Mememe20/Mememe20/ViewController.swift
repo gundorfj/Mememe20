875fb9e2d97a3036319687c33a81f1d4bbd78502
Jan Gundorf <jgundorf@gnresound.com>
2019-03-23 12:59:47 +0100

    Add initial clean project

---
+++ Mememe20/Mememe20/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mememe20
+//
+//  Created by Jan Gundorf on 23/03/2019.
+//  Copyright © 2019 Jan Gundorf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
